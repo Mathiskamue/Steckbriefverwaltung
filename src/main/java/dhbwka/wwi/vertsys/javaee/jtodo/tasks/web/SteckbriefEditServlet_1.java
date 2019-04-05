08e358faf98e296dda7ba4b8d3a10ce279f9d36f
thoma <thoma@172.16.11.76>
2019-04-05 11:25:47 +0200

    Thomas, Mathis-jsp, servlet Änderungen

--- src/main/java/dhbwka/wwi/vertsys/javaee/jtodo/tasks/web/SteckbriefEditServlet_1.java
+++ src/main/java/dhbwka/wwi/vertsys/javaee/jtodo/tasks/web/SteckbriefEditServlet_1.java
@@ -7,10 +7,8 @@
  * Dieser Quellcode ist lizenziert unter einer
  * Creative Commons Namensnennung 4.0 International Lizenz.
  */
+package dhbwka.wwi.vertsys.javaee.jtodo.tasks.web;
 
-
-import dhbw.wwi.deadoralive.ejb.SteckbriefBean;
-import dhbw.wwi.deadoralive.jpa.Steckbrief;
 import dhbwka.wwi.vertsys.javaee.jtodo.common.web.WebUtils;
 import dhbwka.wwi.vertsys.javaee.jtodo.common.web.FormValues;
 import dhbwka.wwi.vertsys.javaee.jtodo.tasks.ejb.CategoryBean;
@@ -41,7 +39,7 @@ import javax.servlet.http.HttpSession;
 public class SteckbriefEditServlet extends HttpServlet {
 
     @EJB
-    SteckbriefBean steckbriefBean;
+    TaskBean taskBean;
 
     @EJB
     CategoryBean categoryBean;
@@ -63,19 +61,19 @@ public class SteckbriefEditServlet extends HttpServlet {
         // Zu bearbeitende Aufgabe einlesen
         HttpSession session = request.getSession();
 
-        Steckbrief steckbrief = this.getRequestedSteckbrief(request);
-        request.setAttribute("edit", steckbrief.getId() != 0);
+        Task task = this.getRequestedTask(request);
+        request.setAttribute("edit", task.getId() != 0);
                                 
-        if (session.getAttribute("steckbrief_form") == null) {
+        if (session.getAttribute("task_form") == null) {
             // Keine Formulardaten mit fehlerhaften Daten in der Session,
             // daher Formulardaten aus dem Datenbankobjekt übernehmen
-            request.setAttribute("steckbrief_form", this.createSteckbriefForm(steckbrief));
+            request.setAttribute("task_form", this.createTaskForm(task));
         }
 
         // Anfrage an die JSP weiterleiten
-        request.getRequestDispatcher("/WEB-INF/steckbrief/steckbrief_edit.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/tasks/task_edit.jsp").forward(request, response);
         
-        session.removeAttribute("steckbrief_form");
+        session.removeAttribute("task_form");
     }
 
     @Override
@@ -91,10 +89,10 @@ public class SteckbriefEditServlet extends HttpServlet {
 
         switch (action) {
             case "save":
-                this.saveSteckbrief(request, response);
+                this.saveTask(request, response);
                 break;
             case "delete":
-                this.deleteSteckbrief(request, response);
+                this.deleteTask(request, response);
                 break;
         }
     }
@@ -107,58 +105,64 @@ public class SteckbriefEditServlet extends HttpServlet {
      * @throws ServletException
      * @throws IOException
      */
-    private void saveSteckbrief(HttpServletRequest request, HttpServletResponse response)
+    private void saveTask(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         // Formulareingaben prüfen
         List<String> errors = new ArrayList<>();
 
-        String steckbriefCategory = request.getParameter("steckbrief_category");
-        String steckbriefDueDate = request.getParameter("steckbrief_due_date");
-    
-        String steckbriefStatus = request.getParameter("steckbrief_status");
-        String steckbriefName = request.getParameter("steckbrief_name");
-        String steckbriefPersonenBeschreibung = request.getParameter("task_personen_beschreibung");
+        String taskCategory = request.getParameter("task_category");
+        String taskDueDate = request.getParameter("task_due_date");
+        String taskDueTime = request.getParameter("task_due_time");
+        String taskStatus = request.getParameter("task_status");
+        String taskShortText = request.getParameter("task_short_text");
+        String taskLongText = request.getParameter("task_long_text");
 
-        Steckbrief steckbrief = this.getRequestedSteckbrief(request);
+        Task task = this.getRequestedTask(request);
 
-        if (steckbriefCategory != null && !steckbriefCategory.trim().isEmpty()) {
+        if (taskCategory != null && !taskCategory.trim().isEmpty()) {
             try {
-                steckbrief.setCategory(this.categoryBean.findById(Long.parseLong(steckbriefCategory)));
+                task.setCategory(this.categoryBean.findById(Long.parseLong(taskCategory)));
             } catch (NumberFormatException ex) {
                 // Ungültige oder keine ID mitgegeben
             }
         }
 
-        Date dueDate = WebUtils.parseDate(steckbriefDueDate);
+        Date dueDate = WebUtils.parseDate(taskDueDate);
+        Time dueTime = WebUtils.parseTime(taskDueTime);
 
         if (dueDate != null) {
-            steckbrief.setDueDate(dueDate);
+            task.setDueDate(dueDate);
         } else {
             errors.add("Das Datum muss dem Format dd.mm.yyyy entsprechen.");
         }
 
-      
+        if (dueTime != null) {
+            task.setDueTime(dueTime);
+        } else {
+            errors.add("Die Uhrzeit muss dem Format hh:mm:ss entsprechen.");
+        }
+
         try {
-            steckbrief.setStatus(TaskStatus.valueOf(steckbriefStatus));
+            task.setStatus(TaskStatus.valueOf(taskStatus));
         } catch (IllegalArgumentException ex) {
             errors.add("Der ausgewählte Status ist nicht vorhanden.");
         }
 
-        steckbrief.setPersonenBeschreibung(steckbriefName);
-        steckbrief.setPersonenBeschreibung(steckbriefPersonenBeschreibung);
+        task.setShortText(taskShortText);
+        task.setLongText(taskLongText);
 
-        this.validationBean.validate(steckbrief, errors);
+        this.validationBean.validate(task, errors);
 
         // Datensatz speichern
         if (errors.isEmpty()) {
-            this.steckbriefBean.update(steckbrief);
+            this.taskBean.update(task);
         }
 
         // Weiter zur nächsten Seite
         if (errors.isEmpty()) {
             // Keine Fehler: Startseite aufrufen
-            response.sendRedirect(WebUtils.appUrl(request, "/app/steckbrief/list/"));
+            response.sendRedirect(WebUtils.appUrl(request, "/app/tasks/list/"));
         } else {
             // Fehler: Formuler erneut anzeigen
             FormValues formValues = new FormValues();
@@ -166,7 +170,7 @@ public class SteckbriefEditServlet extends HttpServlet {
             formValues.setErrors(errors);
 
             HttpSession session = request.getSession();
-            session.setAttribute("steckbrief_form", formValues);
+            session.setAttribute("task_form", formValues);
 
             response.sendRedirect(request.getRequestURI());
         }
@@ -180,15 +184,15 @@ public class SteckbriefEditServlet extends HttpServlet {
      * @throws ServletException
      * @throws IOException
      */
-    private void deleteSteckbrief(HttpServletRequest request, HttpServletResponse response)
+    private void deleteTask(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         // Datensatz löschen
-        Steckbrief steckbrief = this.getRequestedSteckbrief(request);
-        this.steckbriefBean.delete(steckbrief);
+        Task task = this.getRequestedTask(request);
+        this.taskBean.delete(task);
 
         // Zurück zur Übersicht
-        response.sendRedirect(WebUtils.appUrl(request, "/app/steckbrief/list/"));
+        response.sendRedirect(WebUtils.appUrl(request, "/app/tasks/list/"));
     }
 
     /**
@@ -199,33 +203,34 @@ public class SteckbriefEditServlet extends HttpServlet {
      * @param request HTTP-Anfrage
      * @return Zu bearbeitende Aufgabe
      */
-    private Steckbrief getRequestedSteckbrief(HttpServletRequest request) {
+    private Task getRequestedTask(HttpServletRequest request) {
         // Zunächst davon ausgehen, dass ein neuer Satz angelegt werden soll
-        Steckbrief steckbrief = new Steckbrief();
-        steckbrief.setOwner(this.userBean.getCurrentUser());
-        steckbrief.setDueDate(new Date(System.currentTimeMillis()));
+        Task task = new Task();
+        task.setOwner(this.userBean.getCurrentUser());
+        task.setDueDate(new Date(System.currentTimeMillis()));
+        task.setDueTime(new Time(System.currentTimeMillis()));
 
         // ID aus der URL herausschneiden
-        String steckbriefId = request.getPathInfo();
+        String taskId = request.getPathInfo();
 
-        if (steckbriefId == null) {
-            steckbriefId= "";
+        if (taskId == null) {
+            taskId = "";
         }
 
-        steckbriefId= steckbriefId.substring(1);
+        taskId = taskId.substring(1);
 
-        if (steckbriefId.endsWith("/")) {
-            steckbriefId = steckbriefId.substring(0, steckbriefId.length() - 1);
+        if (taskId.endsWith("/")) {
+            taskId = taskId.substring(0, taskId.length() - 1);
         }
 
         // Versuchen, den Datensatz mit der übergebenen ID zu finden
         try {
-            steckbrief = this.steckbriefBean.findById(Long.parseLong(steckbriefId));
+            task = this.taskBean.findById(Long.parseLong(taskId));
         } catch (NumberFormatException ex) {
             // Ungültige oder keine ID in der URL enthalten
         }
 
-        return steckbrief;
+        return task;
     }
 
     /**
@@ -238,34 +243,37 @@ public class SteckbriefEditServlet extends HttpServlet {
      * @param task Die zu bearbeitende Aufgabe
      * @return Neues, gefülltes FormValues-Objekt
      */
-    private FormValues createSteckbriefForm(Steckbrief steckbrief) {
+    private FormValues createTaskForm(Task task) {
         Map<String, String[]> values = new HashMap<>();
 
-        values.put("steckbrief_owner", new String[]{
-            steckbrief.getOwner().getUsername()
+        values.put("task_owner", new String[]{
+            task.getOwner().getUsername()
         });
 
-        if (steckbrief.getCategory() != null) {
-            values.put("steckbrief_category", new String[]{
-                "" + steckbrief.getCategory().getId()
+        if (task.getCategory() != null) {
+            values.put("task_category", new String[]{
+                "" + task.getCategory().getId()
             });
         }
 
-        values.put("steckbrief_due_date", new String[]{
-            WebUtils.formatDate(steckbrief.getDueDate())
+        values.put("task_due_date", new String[]{
+            WebUtils.formatDate(task.getDueDate())
         });
 
-
-        values.put("steckbrief_status", new String[]{
-            steckbrief.getStatus().toString()
+        values.put("task_due_time", new String[]{
+            WebUtils.formatTime(task.getDueTime())
         });
 
-        values.put("steckbrief_name", new String[]{
-            steckbrief.getName()
+        values.put("task_status", new String[]{
+            task.getStatus().toString()
         });
 
-        values.put("task_personen_beschreibung", new String[]{
-            steckbrief.getPersonenBeschreibung()
+        values.put("task_short_text", new String[]{
+            task.getShortText()
+        });
+
+        values.put("task_long_text", new String[]{
+            task.getLongText()
         });
 
         FormValues formValues = new FormValues();
